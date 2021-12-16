572938572929480babefe429056404567f2088d2
John Alexander Jimenez <jjimenez@asesoftware.com>
2021-12-15 19:12:16 -0500

    Se crea clase Nuevo que funciona como manejadora

---
+++ TiendaServicios.Api.Autor/Aplicacion/Nuevo.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Autor.Modelo;
+using TiendaServicios.Api.Autor.Persistencia;
+
+namespace TiendaServicios.Api.Autor.Aplicacion
+{
+    public class Nuevo
+    {
+        public class Ejecuta : IRequest
+        {
+            public string Nombre { get; set; }
+            public string Apelllido { get; set; }
+            public DateTime? FechaNacimiento { get; set; }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+            public readonly ContextoAutor _contexto;
+            public Manejador(ContextoAutor contexto)
+            {
+                _contexto = contexto;
+            }
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var autorLibro = new AutorLibro
+                {
+                    Nombre = request.Nombre,
+                    FechaNacimiento = request.FechaNacimiento,
+                    Apellido = request.Apelllido,
+                };
+
+                _contexto.AutorLibro.Add(autorLibro);
+                var valor = await _contexto.SaveChangesAsync();
+
+                if (valor > 0)
+                {
+                    return Unit.Value;
+                }
+
+                throw new Exception("No se pudo insertar el Autor");
+            }
+        }
+    }
+}
