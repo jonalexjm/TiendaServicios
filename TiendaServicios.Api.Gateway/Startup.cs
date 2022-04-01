90915104c6726641e022935591f674350edb2e3b
John Alexander Jimenez <jhonalexjm@gmail.com>
2022-04-01 16:00:44 -0500

    Message handler para obtener el tiempo de respueta del api

--- TiendaServicios.Api.Gateway/Startup.cs
+++ TiendaServicios.Api.Gateway/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TiendaServicios.Api.Gateway.MessageHandler;
 
 namespace TiendaServicios.Api.Gateway
 {
@@ -28,7 +29,7 @@ namespace TiendaServicios.Api.Gateway
         {
 
             //services.AddControllers();
-            services.AddOcelot();
+            services.AddOcelot().AddDelegatingHandler<LibroHandler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
